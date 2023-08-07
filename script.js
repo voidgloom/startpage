d88e58ce1751a3e8fdc71aa5a3d35377b15a0ada
ralsayyy <level77zockthd@gmail.com>
2023-08-07 14:40:41 +0200

    lots of changes

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+var bookmarkArray = []
+
 // this is run when the page has fully loaded
 function init() {
-	if (localStorage.getItem("bookmarkCount") === null) {
-		localStorage.setItem("bookmarkCount", "0");
+	if (localStorage.getItem("bookmarkJSON") === null) {
+		localStorage.setItem("bookmarkJSON", "[]");
 	} else {
 		addQuickAccessToPage();
 	}
@@ -11,9 +13,6 @@ function init() {
 			search(document.getElementById("searchbox").value);
 		}
 	});
-	if (localStorage.getItem("webstorage") == "consent") {
-		document.getElementById("webstorage-notice").style.display = "none";
-	}
 }
 
 function search(searchQuery) {
@@ -44,51 +43,51 @@ function search(searchQuery) {
 }
 
 function addPageToQuickAccess(url, name) {
-	var nextBookmarkCount = Number(localStorage.getItem("bookmarkCount")) + 1;
-	localStorage.setItem("bookmark" + localStorage.getItem("bookmarkCount") + "_name", name);
-	localStorage.setItem("bookmark" + localStorage.getItem("bookmarkCount") + "_url", url);
-	localStorage.setItem("bookmarkCount", nextBookmarkCount.toString());
+	let bookmarkArray = JSON.parse(localStorage.getItem("bookmarkJSON"));
+	bookmarkArray.push({"name": name, "url": url});
+	localStorage.setItem("bookmarkJSON", JSON.stringify(bookmarkArray));
 }
 
 function addQuickAccessToPage() {
-	var i = 0;
-	var quickAccessBox = document.getElementById("quickaccess-box");
-	while (i < Number(localStorage.getItem("bookmarkCount"))) {
+	let i = 0;
+	let quickAccessBox = document.getElementById("quickaccess-box");
+	let bookmarkArray = JSON.parse(localStorage.getItem("bookmarkJSON"))
+	bookmarkArray.forEach(element => {
 		// this is the main bookmark div
-		var quickaccessLinkBox = document.createElement("div");
+		let quickaccessLinkBox = document.createElement("div");
 		quickaccessLinkBox.setAttribute("class", "quickaccess-link");
-		quickaccessLinkBox.setAttribute("tr-bookmark-link", localStorage.getItem("bookmark" + i + "_url"));
+		quickaccessLinkBox.setAttribute("tr-bookmark-link", element.url);
 		quickaccessLinkBox.setAttribute("tr-bookmark-purpose", "link");
 		quickaccessLinkBox.setAttribute("onclick", "openBookmark(this)")
 		// this is used for animations, mostly
-		var quickaccessLinkBackground = document.createElement("div");
+		let quickaccessLinkBackground = document.createElement("div");
 		quickaccessLinkBackground.setAttribute("class", "quickaccess-link-background");
 		// this is the text displaying the bookmark name
-		var quickaccessLinkText = document.createElement("p")
+		let quickaccessLinkText = document.createElement("p")
 		quickaccessLinkText.setAttribute("class", "quickaccess-link-text");
-		quickaccessLinkText.innerHTML = localStorage.getItem("bookmark" + i + "_name");
+		quickaccessLinkText.innerHTML = element.name;
 		// now add everything into the quickaccessLinkBox
 		quickaccessLinkBox.insertBefore(quickaccessLinkBackground, quickaccessLinkBox.firstChild);
 		quickaccessLinkBackground.insertBefore(quickaccessLinkText, quickaccessLinkBackground.firstChild);
 		// insert the bookmark into the page
 		quickAccessBox.insertBefore(quickaccessLinkBox, quickAccessBox.lastChild);
-		i++;
-	}
-}
-
-function consent_webstorage() {
-	localStorage.setItem("webstorage", "consent")
-	document.getElementById("webstorage-notice").style.display = "none";
+	});
 }
 
 function dialog_quickaccess_add_dialog_submit() {
-	var bookmarkUrl = document.getElementById("quickaccess-add-dialog-urlinput").value;
-	var bookmarkName = document.getElementById("quickaccess-add-dialog-nameinput").value;
+	let bookmarkUrl = document.getElementById("quickaccess-add-dialog-urlinput").value;
+	let bookmarkName = document.getElementById("quickaccess-add-dialog-nameinput").value;
 	if (bookmarkUrl != "" && bookmarkName != "") {
 		document.getElementById("quickaccess-box").innerHTML = '<div class="quickaccess-link" onclick="dialog_quickaccess_add_dialog_show()" tr-bookmark-purpose="add-bookmark"><div class="quickaccess-link-background"><img class="quickaccess-link-image" src="assets/plus.png" width=64 height=64><p class="quickaccess-link-text">Add Bookmark</p></div></div>';
 		addPageToQuickAccess(bookmarkUrl, bookmarkName);
 		addQuickAccessToPage();
 	}
+	document.getElementById("dialogbox").style.display = "none";
+}
+
+function dialog_quickaccess_add_cancel() {
+	document.getElementById("quickaccess-add-dialog-urlinput").value = "";
+	document.getElementById("quickaccess-add-dialog-nameinput").value = "";
 	document.getElementById("dialogbox").style.display = "none";
 }
 
